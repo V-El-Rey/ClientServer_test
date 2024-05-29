0dfd5001989cb4449fac882b50d5aab9e6e831da
V_ElRey <the.akopyan.valeriy@gmail.com>
2024-05-29 15:04:41 +0300

    Initial commit

--- obj/Debug/net6.0/ClientServer_test.AssemblyInfo.cs
+++ obj/Debug/net6.0/ClientServer_test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientServer_test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e37d1c95f996c929e14744344309f9ffb788e2e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientServer_test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientServer_test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
